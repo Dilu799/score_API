b2019d764e1a572367158c20893e735e246b1be1
dilu799 <dhilshad799@gmail.com>
2021-06-03 00:38:10 +0530

    Front-End Setup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
+var db = require('../config/connection');
+var objectId = require('mongodb').ObjectID;
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home');
 });
+
+router.get('/addcandidate',(req,res)=>{
+  res.render('addcandidate')
+})
+
+router.get('/addscore',(req,res)=>{
+  res.render('addscore')
+})
+
+router.get('/topscorer',(req,res)=>{
+  res.render('topscorer');
+})
+
+router.get('/average',(req,res)=>{
+  res.render('average')
+})
+
+
 
 module.exports = router;
